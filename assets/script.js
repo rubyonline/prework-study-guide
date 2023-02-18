f84e78e186bd2e36b251f7d7dcf9812f6f5e3b8d
Ruby <rubyfredrickson@gmail.com>
2023-02-18 10:06:21 -0600

    Added functions to javascript

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,30 @@
-var topic = "HTML";
+var topic = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topic[Math.floor(Math.random() * topic.length)];
 
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+     console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+     console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+     console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+     console.log('Please try again!');
+}
 }
 
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for(var list = 0; list < topics.length; list++){
-    console.log(topics[list]);
+function listTopics(){
+    for(var list = 0; list < topics.length; list++){
+        console.log(topics[list]);
+    }
 }
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
